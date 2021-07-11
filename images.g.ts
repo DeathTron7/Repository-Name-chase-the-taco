71945a3bbbac93f5dc052a737eb879787440b237
DeathTron7 <87275016+DeathTron7@users.noreply.github.com>
2021-07-11 12:23:41 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,37 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "3n8D*BP{-xoQH6G(a)=|":
+            case "knight":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . c c . . . . . . . 
+. . . . . . c 5 c c . . . . . . 
+. . . . c c c 5 5 5 c c . . . . 
+. . c c b c 5 5 5 5 c c c c . . 
+. c b b 5 b 5 5 5 5 b 5 b b c . 
+. c b 5 5 b b 5 5 b b 5 5 b c . 
+. . f 5 5 5 b b b b 5 5 5 c . . 
+. . f f 5 5 5 5 5 5 5 5 f . . f 
+. . f f e e b f e e e f . . f f 
+. . f f f b 1 f b b e f . . f . 
+. . . f f b b b b b b f . f f . 
+. . e b b e e e e e f . f f . . 
+. . e b b e b b b 5 5 f f . . . 
+. . . e e e 5 5 5 5 5 c . . . . 
+. . . . . f f f f f f . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
